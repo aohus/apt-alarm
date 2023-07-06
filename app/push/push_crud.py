591695f35e1caa08861b85b4da45e0850e8e369a
유수화 <dbtnghk528@gmail.com>
2023-07-06 23:10:04 +0900

    Refactor the notice method in PushController

--- app/push/push_crud.py
+++ app/push/push_crud.py
@@ -19,38 +19,51 @@ morning = datetime.datetime.now().hour == 9
 
 class PushController:
     async def notice(self) -> List:
-        interest_complex_list = await InterestController().read_interest_complex_list(
-            "user_id"
+        interest_complex_list = await self._read_interest_complex_list("user_id")
+        logging.info(interest_complex_list)
+        available_complex_list = await self._get_available_apartments(
+            interest_complex_list
         )
+
         available_apt_list = []
+        for complex in available_complex_list:
+            complex_id = complex[0]["complex_id"]
+            complex_name = complex[0]["complex_name"]
+
+            filtered_apt = await self._check_custom_condition(complex_id, complex)
+            filtered_apt = await self._check_noticed(filtered_apt)
+            available_apt_list.extend(filtered_apt)
+
+            self._send_messages(complex_name, filtered_apt)
+        await self._update_noticed_apt(available_apt_list)
+        return available_apt_list
+
+    async def _read_interest_complex_list(self, user_id: str) -> List:
+        return await InterestController().read_interest_complex_list(user_id)
+
+    async def _get_available_apartments(
+        self, interest_complex_list: InterestModel
+    ) -> List[Dict]:
         available_complex_list = await asyncio.gather(
             *[
                 NaverAPTScraper.get_available_apt(complex.complex_id)
                 for complex in interest_complex_list
             ]
         )
-        for complex in available_complex_list:
-            if complex is None:
-                continue
-            filtered_apt = await self._check_condition(
-                complex[0]["complex_id"], complex
-            )
-            filtered_apt = await self._check_noticed(filtered_apt)
-            available_apt_list.extend(filtered_apt)
+        return [complex for complex in available_complex_list if complex is not None]
 
-            if morning:
-                slackbot.post_apt_message(complex[0]["complex_name"], filtered_apt)
-            elif any(apt["noticed_label"] == ":new:" for apt in filtered_apt):
-                new_apt = [
-                    apt for apt in filtered_apt if apt["noticed_label"] == ":new:"
-                ]
-                slackbot.post_apt_message(complex[0]["complex_name"], new_apt)
-
+    def _send_messages(self, complex_name: str, filtered_apt: List[Dict]):
+        if morning:
+            slackbot.post_apt_message(complex_name, filtered_apt)
             logging.info(
-                f"[PushController.noticed] {complex[0]['complex_name']} push filtered_apt len : {len(filtered_apt)}"
+                f"[PushController.noticed] {complex_name} push filtered_apt len : {len(filtered_apt)}"
             )
-        await self._update_noticed_apt(available_apt_list)
-        return available_apt_list
+        elif any(apt["noticed_label"] == ":new:" for apt in filtered_apt):
+            new_apt = [apt for apt in filtered_apt if apt["noticed_label"] == ":new:"]
+            slackbot.post_apt_message(complex_name, new_apt)
+            logging.info(
+                f"[PushController.noticed] {complex_name} push filtered_apt len : {len(new_apt)}"
+            )
 
     def _convert_price(self, price: str) -> Optional[float]:
         # 숫자와 ','만 추출
@@ -69,7 +82,7 @@ class PushController:
         else:
             return None
 
-    async def _check_condition(
+    async def _check_custom_condition(
         self, complex_id: int, available_apt: List[Dict]
     ) -> List[Dict]:
         interest_model = await mongodb.engine.find_one(
