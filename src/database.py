acb7d2d1e01cd8e37dd18a15d1c975470b8b3e5d
유수화 <dbtnghk528@gmail.com>
2024-01-19 00:27:24 +0900

    fix: mongo db connectino

--- src/database.py
+++ src/database.py
@@ -6,25 +6,34 @@ from odmantic import AIOEngine
 
 
 class MongoDB:
-    def __init__(self):
-        self.client = None
-        self.engine = None
-        self.MONGO_URL = os.getenv("MONGO_DB_URL")
-        self.MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
-        self.MONGO_MAX_CONNECTIONS = os.getenv("MONGO_MAX_CONNECTIONS")
-        self.MONGO_MIN_CONNECTIONS = os.getenv("MONGO_MIN_CONNECTIONS")
+    MONGO_URL = os.getenv("MONGO_DB_URL")
+    MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+    MONGO_MAX_CONNECTIONS = os.getenv("MONGO_MAX_CONNECTIONS")
+    MONGO_MIN_CONNECTIONS = os.getenv("MONGO_MIN_CONNECTIONS")
 
-    def connect(self):
-        self.client = AsyncIOMotorClient(
+    def __init__(self) -> None:
+        self.__client: AsyncIOMotorClient = None
+        self.__engine: AIOEngine = None
+
+    @property
+    def client(self) -> AsyncIOMotorClient:
+        return self.__client
+
+    @property
+    def engine(self) -> AIOEngine:
+        return self.__engine
+
+    async def connect(self):
+        self.__client = AsyncIOMotorClient(
             self.MONGO_URL,
             maxPoolSize=self.MONGO_MAX_CONNECTIONS,
             minPoolSize=self.MONGO_MIN_CONNECTIONS,
         )
-        self.engine = AIOEngine(client=self.client, database=self.MONGO_DB_NAME)
+        self.__engine = AIOEngine(client=self.__client, database=self.MONGO_DB_NAME)
         logging.info("DB와 성공적으로 연결이 되었습니다.")
 
-    def close(self):
-        self.client.close()
+    async def close(self):
+        self.__client.close()
 
 
 mongodb = MongoDB()
